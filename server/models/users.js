55122682635e8160f1a5178d55bdfd67118f9682
gmzelias <gmzelias@gmail.com>
2018-02-23 16:25:39 -0400

    Add POST /users route and token method

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,59 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ =  require('lodash');
 
-var User = mongoose.model('Users',{
+var UserSchema = new mongoose.Schema({   
     email:{
-        type:String,
-        required: true,
-        minLenght:1,
-        trim: true
+    type:String,
+    required: true,
+    minLenght:1,
+    trim: true,
+    unique:true,
+    validate:{
+        validator: (value)=>{
+            return validator.isEmail(value);
+        },
+        message: '{VALUE} is not a valid email'
     }
+},
+password:{
+    type:String,
+    require:true,
+    minlength: 6
+},
+tokens: [{
+    access:{
+        type:String,
+        required:true
+    },
+    token:{
+        type:String,
+        required:true
+    }
+}]
 });
+
+UserSchema.methods.toJSON = function (){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id','email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+var user = this;
+var access = 'auth';
+var token = jwt.sign({_id:user._id.toHexString(), access : access},'abc123').toString();
+console.log(token);
+//user.tokens = user.tokens.concat([{acess,token}]);
+user.tokens.push({access,token});
+
+return user.save().then(()=>{
+    return token;
+});
+}
+
+var User = mongoose.model('Users',UserSchema);
 
 module.exports = {User:User};
--- server/server.js
+++ server/server.js
@@ -116,7 +116,21 @@ app.patch('/todos/:id',(req,res)=>{
     })
 
 
-})
+});
+
+app.post('/users',(req,res)=>{
+    var body= _.pick(req.body,['email','password']);
+    var user = new User (body);
+    user.save().then(()=>{
+        //res.send(user); chain promises
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=>{
+    res.status(400).send(e);
+    });
+});
+
 
 
 
